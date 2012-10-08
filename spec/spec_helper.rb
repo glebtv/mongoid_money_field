a28e7ea95288f3a277297eb4e32cc8b830a289e5
Gleb Tv <glebtv@gmail.com>
2012-10-08 10:49:11 +0400

    Attemp to fix CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,8 +15,18 @@ Money.default_currency = Money::Currency.new("RUB")
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-Mongoid.load!("#{File.dirname(__FILE__)}/mongoid.yml", :test)
-Mongoid.logger = Logger.new($stdout)
+
+def mongoid3?
+  defined?(Mongoid::VERSION) && Gem::Version.new(Mongoid::VERSION) >= Gem::Version.new('3.0.0.rc')
+end
+
+Mongoid.configure do |config|
+  if mongoid3?
+    config.sessions[:default] = { :database => 'mongoid_money_field_test', :hosts => ['localhost:27017'] }
+  else
+    config.master = Mongo::Connection.new.db('mongoid_money_field_test')
+  end
+end
 
 DatabaseCleaner.orm = "mongoid"
 
