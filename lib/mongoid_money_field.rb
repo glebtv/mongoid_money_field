c1bb334c62e28d858f8142819691b6bb20b6bc41
xronos-i-am <xronos.i.am@gmail.com>
2012-11-27 17:54:03 +0400

    добавил инпут :money для SimpleForm поле money поддерживает валидацию Numericality

--- lib/mongoid_money_field.rb
+++ lib/mongoid_money_field.rb
@@ -1,45 +1,74 @@
 # coding: utf-8
 
+module SimpleForm
+  module Inputs
+    class MoneyInput < Base
+      enable :placeholder, :min_max
+
+      def input
+        add_size!
+        input_html_classes.unshift("numeric")
+        if html5?
+          input_html_options[:type] ||= "number"
+          input_html_options[:step] ||= integer? ? 1 : "any"
+        end
+        @builder.text_field("#{attribute_name}_plain", input_html_options)
+      end
+
+      private
+
+      # Rails adds the size attr by default, if the :size key does not exist.
+      def add_size!
+        input_html_options[:size] ||= nil
+      end
+    end
+  end
+end
+
 require 'money'
 
 module Mongoid
   module MoneyField
     extend ActiveSupport::Concern
+    
     included do
-
     end
 
     module ClassMethods
 
-      def money_field(*columns)
-        opts = columns.last.is_a?(Hash) ? columns.pop : {}
+      def money_field( *columns )
+        opts = columns.last.is_a?( Hash ) ? columns.pop : {}
         opts = {
-            fixed_currency: nil,
-            default: nil,
-            required: false,
-            default_currency: nil
-        }.merge(opts)
+          fixed_currency: nil,
+          default: nil,
+          required: false,
+          default_currency: nil
+        }.merge( opts )
 
-        [columns].flatten.each do |name|
-          default_money = nil
+        [ columns ].flatten.each do |name|
+          default, default_cents = nil, nil
 
-          if opts[:default].nil?
-            default = nil
-          else
-            default_money = Money.parse(opts[:default])
-            default = default_money.cents
+          name = name.to_s
+
+          unless opts[:default].nil?
+            default = Money.parse( opts[:default] )
+            default_cents = default.cents
           end
 
-          attr_cents = (name.to_s + '_cents').to_sym
-          attr_currency = (name.to_s + '_currency').to_sym
+          attr_cents            = name + '_cents'
+          attr_currency         = name + '_currency'
+          attr_plain            = name + '_plain'
+          attr_before_type_cast = name + '_before_type_cast'
 
-          field attr_cents, type: Integer, default: default
+          field attr_cents, type: Integer, default: default_cents
+
+          attr_accessible attr_plain
+          
           if opts[:fixed_currency].nil?
             default_currency = nil
+
             if opts[:default_currency].nil?
-              unless default_money.nil?
-                default_currency = default_money.currency.iso_code
-              end
+              default_currency = default.currency.iso_code unless default.nil?
             else
               default_currency = opts[:default_currency]
             end
@@ -47,59 +76,58 @@ module Mongoid
             field attr_currency, type: String, default: default_currency
           end
 
-          if opts[:required]
-            validate do
-              cents = read_attribute(attr_cents)
+          # преобразования перед валидацией
+          define_method( attr_before_type_cast ) do
+            code = opts[:fixed_currency].nil? ? read_attribute( attr_currency ) : opts[:fixed_currency]
+            currency = Money::Currency.find( code )
 
-              if cents.nil?
-                errors.add(name, errors.generate_message(name, :error, default: "invalid value for #{name}"))
-              end
-              if opts[:fixed_currency].nil? && read_attribute(attr_currency).nil?
-                errors.add(name, errors.generate_message(name, :error, default: "invalid value for #{name} currency"))
-              end
-            end
+            value = instance_variable_get( "@#{attr_plain}".to_sym )
+
+            value = value.gsub( currency.thousands_separator, '' ).gsub( currency.decimal_mark, '.' )
           end
 
+          # значение поля типа Money
+          define_method( name ) do
+            cents = read_attribute( attr_cents )
 
-          define_method(name) do
-            cents = read_attribute(attr_cents)
+            code = opts[:fixed_currency].nil? ? read_attribute( attr_currency ) : opts[:fixed_currency]
 
-            if opts[:fixed_currency].nil?
-              currency = read_attribute(attr_currency)
-            else
-              currency = opts[:fixed_currency]
-            end
-
-            if cents.nil?
-              nil
-            else
-              Money.new(cents, currency || ::Money.default_currency)
-            end
+            cents.nil? ? nil : Money.new( cents, code || ::Money.default_currency )
           end
+          
+          # строковое значение поля
+          define_method( attr_plain ) do
+            value = instance_variable_get( "@#{attr_plain}".to_sym )
+            value = self.send( name ) if value.nil?
+            value = value.format( symbol: false, no_cents_if_whole: true ) if value.is_a?( Money )
 
-          define_method("#{name}=") do |value|
+            value
+          end
+          
+          define_method( "#{attr_plain}=" ) do |value|
+            instance_variable_set( "@#{attr_plain}".to_sym, value )
+            
             if value.blank?
-              write_attribute(attr_cents, nil)
-              if opts[:fixed_currency].nil?
-                write_attribute(attr_currency, nil)
-              end
-              nil
-            else
-              if opts[:default_currency].nil?
-                money = value.to_money
-              else
-                old_default = Money.default_currency
-                Money.default_currency = Money::Currency.new(opts[:default_currency])
-                money = value.to_money
-                Money.default_currency = old_default
-              end
-
-              write_attribute(attr_cents, money.cents)
-              if opts[:fixed_currency].nil?
-                write_attribute(attr_currency, money.currency.iso_code)
-              end
-              money
+              write_attribute( attr_cents, nil )
+              write_attribute( attr_currency, nil ) if opts[:fixed_currency].nil?
+              
+              return
             end
+
+            value = self.send( attr_before_type_cast )
+
+            if opts[:default_currency].nil?
+              money = value.to_money
+
+            else
+              old_default = Money.default_currency
+              Money.default_currency = Money::Currency.new( opts[:default_currency] )
+              money = value.to_money
+              Money.default_currency = old_default
+            end
+
+            write_attribute( attr_cents, money.cents )
+            write_attribute( attr_currency, money.currency.iso_code ) if opts[:fixed_currency].nil?
           end
         end
       end
