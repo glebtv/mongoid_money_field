802e909e78e555650bbca40d768e6b4fcc47ac47
xronos-i-am <xronos.i.am@gmail.com>
2012-11-28 13:41:31 +0400

    условная подгрузка simple_form/money_input

--- lib/mongoid_money_field.rb
+++ lib/mongoid_money_field.rb
@@ -111,23 +111,6 @@ module Mongoid
   end
 end
 
-class MoneyInput < SimpleForm::Inputs::Base
-  enable :placeholder, :min_max
-
-  def input
-    add_size!
-    input_html_classes.unshift("numeric")
-    if html5?
-      input_html_options[:type] ||= "number"
-      input_html_options[:step] ||= integer? ? 1 : "any"
-    end
-    @builder.text_field("#{attribute_name}_plain", input_html_options)
-  end
-
-  private
-
-  # Rails adds the size attr by default, if the :size key does not exist.
-  def add_size!
-    input_html_options[:size] ||= nil
-  end
+if Object.const_defined?("SimpleForm")
+  require "mongoid_money_field/simple_form/money_input"
 end
