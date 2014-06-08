e5b328060e0534a22d7c00a0375d83c05517a3a3
glebtv <glebtv@gmail.com>
2014-06-08 18:58:49 +0400

    transpec

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -8,16 +8,16 @@ describe Mongoid::MoneyField do
     it 'should be valid to save when field is filled in' do
       dummy       = DummyMoneyRequired.new
       dummy.price = '$10'
-      dummy.should be_valid
-      dummy.save.should eq true
+      expect(dummy).to be_valid
+      expect(dummy.save).to eq true
     end
 
     it 'should be not valid to save when field is not filled in' do
       dummy = DummyMoneyRequired.new
-      dummy.should_not be_valid
-      dummy.errors.count.should eq 1
-      dummy.errors.messages[:price][0].should eq "can't be blank"
-      dummy.save.should eq false
+      expect(dummy).not_to be_valid
+      expect(dummy.errors.count).to eq 1
+      expect(dummy.errors.messages[:price][0]).to eq "can't be blank"
+      expect(dummy.save).to eq false
     end
   end
 
@@ -25,35 +25,35 @@ describe Mongoid::MoneyField do
     it 'should raise the error when value consists non digits' do
       dummy       = DummyNotANumber.new
       dummy.price = 'incorrect1'
-      dummy.should_not be_valid
-      dummy.errors.count.should eq 1
-      dummy.errors.messages[:price][0].should eq "is not a number"
-      dummy.save.should eq false
+      expect(dummy).not_to be_valid
+      expect(dummy.errors.count).to eq 1
+      expect(dummy.errors.messages[:price][0]).to eq "is not a number"
+      expect(dummy.save).to eq false
     end
 
     it 'should raise the error when value consists more then one decimal separator' do
       dummy       = DummyNotANumber.new
       dummy.price = '121,212,22'
-      dummy.should_not be_valid
-      dummy.errors.count.should eq 1
-      dummy.errors.messages[:price][0].should eq "is not a number"
-      dummy.save.should eq false
+      expect(dummy).not_to be_valid
+      expect(dummy.errors.count).to eq 1
+      expect(dummy.errors.messages[:price][0]).to eq "is not a number"
+      expect(dummy.save).to eq false
     end
 
     it 'should raise the error when value is not present' do
       dummy = DummyNotANumber.new
-      dummy.should_not be_valid
-      dummy.errors.count.should eq 1
-      dummy.errors.messages[:price][0].should eq "is not a number"
-      dummy.save.should eq false
+      expect(dummy).not_to be_valid
+      expect(dummy.errors.count).to eq 1
+      expect(dummy.errors.messages[:price][0]).to eq "is not a number"
+      expect(dummy.save).to eq false
     end
 
     it 'should raise the error when value is not present' do
       dummy = DummyNotANumber.new(price: '')
-      dummy.should_not be_valid
-      dummy.errors.count.should eq 1
-      dummy.errors.messages[:price][0].should eq "is not a number"
-      dummy.save.should eq false
+      expect(dummy).not_to be_valid
+      expect(dummy.errors.count).to eq 1
+      expect(dummy.errors.messages[:price][0]).to eq "is not a number"
+      expect(dummy.save).to eq false
     end
   end
 
@@ -61,78 +61,78 @@ describe Mongoid::MoneyField do
     it 'should raise the error when value consists non digits' do
       dummy       = DummyNotANumber.new
       dummy.price = 'incorrect1'
-      dummy.should_not be_valid
-      dummy.errors.count.should eq 1
-      dummy.errors.messages[:price][0].should eq "is not a number"
-      dummy.save.should eq false
+      expect(dummy).not_to be_valid
+      expect(dummy.errors.count).to eq 1
+      expect(dummy.errors.messages[:price][0]).to eq "is not a number"
+      expect(dummy.save).to eq false
     end
 
     it 'should raise the error when value consists more then one decimal separator' do
       dummy       = DummyNotANumber.new
       dummy.price = '121,212,22'
-      dummy.should_not be_valid
-      dummy.errors.count.should eq 1
-      dummy.errors.messages[:price][0].should eq "is not a number"
-      dummy.save.should eq false
+      expect(dummy).not_to be_valid
+      expect(dummy.errors.count).to eq 1
+      expect(dummy.errors.messages[:price][0]).to eq "is not a number"
+      expect(dummy.save).to eq false
     end
   end
 
   describe 'when value should be a positive number' do
     it 'should raise the error when value lesser than 1' do
       dummy = DummyPositiveNumber.new(price: '-10')
-      dummy.should_not be_valid
-      dummy.errors.count.should eq 1
-      dummy.errors.messages[:price][0].should eq "must be greater than 1"
-      dummy.save.should eq false
+      expect(dummy).not_to be_valid
+      expect(dummy.errors.count).to eq 1
+      expect(dummy.errors.messages[:price][0]).to eq "must be greater than 1"
+      expect(dummy.save).to eq false
     end
 
     it 'should raise the error when value lesser than 1' do
       dummy = DummyPositiveNumber.new(price: '-1000')
-      dummy.should_not be_valid
-      dummy.errors.count.should eq 1
-      dummy.errors.messages[:price][0].should eq "must be greater than 1"
-      dummy.save.should eq false
+      expect(dummy).not_to be_valid
+      expect(dummy.errors.count).to eq 1
+      expect(dummy.errors.messages[:price][0]).to eq "must be greater than 1"
+      expect(dummy.save).to eq false
     end
 
     it 'should raise the error when value lesser than 1' do
       dummy = DummyPositiveNumber.new(price: '0')
-      dummy.should_not be_valid
-      dummy.errors.count.should eq 1
-      dummy.errors.messages[:price][0].should eq "must be greater than 1"
-      dummy.save.should eq false
+      expect(dummy).not_to be_valid
+      expect(dummy.errors.count).to eq 1
+      expect(dummy.errors.messages[:price][0]).to eq "must be greater than 1"
+      expect(dummy.save).to eq false
     end
 
     it 'should be ok when value is greater than 1' do
       dummy = DummyPositiveNumber.new(price: '10')
-      dummy.should be_valid
-      dummy.save.should eq true
+      expect(dummy).to be_valid
+      expect(dummy.save).to eq true
     end
 
     it 'should be ok when value is greater than 1' do
       dummy = DummyPositiveNumber.new(price: '1000')
-      dummy.should be_valid
-      dummy.save.should eq true
+      expect(dummy).to be_valid
+      expect(dummy.save).to eq true
     end
 
     it 'should be ok when value is not present' do
       dummy = DummyPositiveNumber.new(price: '')
-      dummy.should be_valid
-      dummy.save.should eq true
+      expect(dummy).to be_valid
+      expect(dummy.save).to eq true
     end
   end
 
   describe 'when both default currency and fixed currency is specified' do
     it 'should use fixed currency instead of default' do
       DummyOverrideDefaultCurrency.create!(price: '1.23')
-      DummyOverrideDefaultCurrency.first.price.currency.iso_code.should eq 'GBP'
+      expect(DummyOverrideDefaultCurrency.first.price.currency.iso_code).to eq 'GBP'
     end
   end
 
   describe 'when default currency is specified' do
     it 'should use it instead of Money.default_currency' do
       DummyWithDefaultCurrency.create!(price: '1.23')
-      DummyWithDefaultCurrency.first.price.currency.iso_code.should eq 'EUR'
-      Money.default_currency.iso_code.should eq 'RUB'
+      expect(DummyWithDefaultCurrency.first.price.currency.iso_code).to eq 'EUR'
+      expect(Money.default_currency.iso_code).to eq 'RUB'
     end
   end
 
@@ -140,32 +140,32 @@ describe Mongoid::MoneyField do
     it 'should be persisted normally when set as dollars' do
       dummy       = DummyMoney.new
       dummy.price = '$10'
-      dummy.save.should eq true
+      expect(dummy.save).to eq true
     end
 
     it 'should be persisted normally when set as cents' do
       dummy       = DummyMoney.new
       dummy.price = '$9.99'
-      dummy.save.should eq true
+      expect(dummy.save).to eq true
     end
 
     it 'should be persisted normally when set as Money' do
       dummy       = DummyMoney.new
       dummy.price = Monetize.parse(1.23)
-      dummy.save.should eq true
+      expect(dummy.save).to eq true
     end
 
     it 'should be possible to set value to nil' do
       dummy       = DummyMoney.new
       dummy.price = Monetize.parse(1.23)
-      dummy.save.should eq true
+      expect(dummy.save).to eq true
 
       dummy = DummyMoney.first
-      dummy.price.should eq Monetize.parse(1.23)
+      expect(dummy.price).to eq Monetize.parse(1.23)
       dummy.price = nil
-      dummy.save.should eq true
+      expect(dummy.save).to eq true
       dummy = DummyMoney.first
-      dummy.price.should be_nil
+      expect(dummy.price).to be_nil
     end
   end
 
@@ -176,7 +176,7 @@ describe Mongoid::MoneyField do
 
     it 'should have a Money value that matches the money value that was initially persisted' do
       dummy = DummyMoney.first
-      dummy.price.should eq Monetize.parse('9.99')
+      expect(dummy.price).to eq Monetize.parse('9.99')
     end
   end
 
@@ -189,7 +189,7 @@ describe Mongoid::MoneyField do
 
     it 'should have a Money value that matches the money value that was initially persisted' do
       dummy = DummyMoney.first
-      dummy.price.cents.should eq 123
+      expect(dummy.price.cents).to eq 123
     end
   end
 
@@ -200,50 +200,50 @@ describe Mongoid::MoneyField do
 
     it 'should have a Money value that matches the money value that was initially persisted' do
       dummy = DummyMoney.first
-      dummy.price.cents.should eq 123
+      expect(dummy.price.cents).to eq 123
     end
   end
 
   describe 'when accessing a document from the datastore with a Money datatype and empty value' do
     it 'should be nil' do
       dummy = DummyMoneyWithoutDefault.new
-      dummy.save.should eq true
-      DummyMoneyWithoutDefault.first.price.should be_nil
+      expect(dummy.save).to eq true
+      expect(DummyMoneyWithoutDefault.first.price).to be_nil
     end
 
     it 'should be 0 when used with default' do
       dummy = DummyMoneyWithDefault.new
-      dummy.save.should eq true
-      DummyMoneyWithDefault.first.price.cents.should eq 100
+      expect(dummy.save).to eq true
+      expect(DummyMoneyWithDefault.first.price.cents).to eq 100
     end
 
     it 'should set money to default currency if money is given without it' do
       dummy = DummyMoneyWithDefault.new
-      dummy.save.should eq true
+      expect(dummy.save).to eq true
       dummy = DummyMoneyWithDefault.first
-      dummy.price.currency.iso_code.should eq Money.default_currency.iso_code
-      dummy.price.cents.should eq 100
+      expect(dummy.price.currency.iso_code).to eq Money.default_currency.iso_code
+      expect(dummy.price.cents).to eq 100
     end
 
     it 'should set money to default currency if money is given without it on a document with multiple money fields' do
       dummy = DummyPrices.new
-      dummy.save.should eq true
+      expect(dummy.save).to eq true
       dummy = DummyPrices.first
-      dummy.price.currency.iso_code.should eq Money.default_currency.iso_code
-      dummy.price.cents.should eq 100
+      expect(dummy.price.currency.iso_code).to eq Money.default_currency.iso_code
+      expect(dummy.price.cents).to eq 100
 
-      dummy.price2.should be_nil
+      expect(dummy.price2).to be_nil
 
-      dummy.price1.cents.should eq 0
+      expect(dummy.price1.cents).to eq 0
     end
 
 
     it 'should set money to correct currency if money is given with it' do
       dummy = DummyMoneyWithDefaultWithCurrency.new
-      dummy.save.should eq true
+      expect(dummy.save).to eq true
       dummy = DummyMoneyWithDefaultWithCurrency.first
-      dummy.price.currency.iso_code.should eq 'GBP'
-      dummy.price.cents.should eq 100
+      expect(dummy.price.currency.iso_code).to eq 'GBP'
+      expect(dummy.price.cents).to eq 100
     end
   end
 
@@ -251,24 +251,24 @@ describe Mongoid::MoneyField do
     it 'should have correct currency when value is set to 5$' do
       DummyMoneyWithFixedCurrency.create!(price: '5$')
       dummy = DummyMoneyWithFixedCurrency.first
-      dummy.price.currency.iso_code.should eq 'GBP'
-      dummy.price.cents.should eq 500
-      dummy.price.should eq Monetize.parse('5 GBP')
+      expect(dummy.price.currency.iso_code).to eq 'GBP'
+      expect(dummy.price.cents).to eq 500
+      expect(dummy.price).to eq Monetize.parse('5 GBP')
     end
 
     it 'works for other field' do
       DummyMoneyWithFixedCurrency.create!(old_price: '5$', price: '')
       dummy = DummyMoneyWithFixedCurrency.first
-      dummy.price.should be_nil
-      dummy.old_price.cents.should eq 500
+      expect(dummy.price).to be_nil
+      expect(dummy.old_price.cents).to eq 500
     end
 
     it 'should have correct currency when value is set to 100 RUB' do
       DummyMoneyWithFixedCurrency.create!(price: '100 RUB')
       dummy = DummyMoneyWithFixedCurrency.first
-      dummy.price.currency.iso_code.should eq 'GBP'
-      dummy.price.cents.should eq 100_00
-      dummy.price.should eq Monetize.parse('100 GBP')
+      expect(dummy.price.currency.iso_code).to eq 'GBP'
+      expect(dummy.price.cents).to eq 100_00
+      expect(dummy.price).to eq Monetize.parse('100 GBP')
     end
   end
 
@@ -276,17 +276,17 @@ describe Mongoid::MoneyField do
     it 'should handle RUB' do
       DummyMoney.create(description: "Test", price: '1.23 RUB')
       dummy = DummyMoney.first
-      dummy.price.currency.iso_code.should eq 'RUB'
-      dummy.price.cents.should eq 123
-      dummy.price.should eq Monetize.parse('1.23 RUB')
+      expect(dummy.price.currency.iso_code).to eq 'RUB'
+      expect(dummy.price.cents).to eq 123
+      expect(dummy.price).to eq Monetize.parse('1.23 RUB')
     end
 
     it 'should handle $' do
       DummyMoney.create(description: "Test", price: '1.23 USD')
       dummy = DummyMoney.first
-      dummy.price.currency.iso_code.should eq 'USD'
-      dummy.price.cents.should eq 123
-      dummy.price.should eq Monetize.parse('1.23 USD')
+      expect(dummy.price.currency.iso_code).to eq 'USD'
+      expect(dummy.price.cents).to eq 123
+      expect(dummy.price).to eq Monetize.parse('1.23 USD')
     end
   end
 
@@ -297,25 +297,25 @@ describe Mongoid::MoneyField do
 
     it 'should be nil' do
       dummy = DummyMoney.first
-      dummy.price.should be_nil
+      expect(dummy.price).to be_nil
     end
 
     it 'stays nil' do
       dummy = DummyMoney.first
       dummy.price = ''
-      dummy.price.should be_nil
+      expect(dummy.price).to be_nil
       expect(dummy.save).to be_truthy
-      DummyMoney.first.price.should be_nil
+      expect(DummyMoney.first.price).to be_nil
     end
 
     it 'should be updated correctly' do
       dummy = DummyMoney.first
-      dummy.price.should be_nil
+      expect(dummy.price).to be_nil
       dummy.price = '1.23 USD'
-      dummy.save.should eq true
+      expect(dummy.save).to eq true
       dummy = DummyMoney.first
-      dummy.price.currency.iso_code.should eq 'USD'
-      dummy.price.cents.should eq 123
+      expect(dummy.price.currency.iso_code).to eq 'USD'
+      expect(dummy.price.cents).to eq 123
     end
   end
 
@@ -332,12 +332,12 @@ describe Mongoid::MoneyField do
 
     it 'should have correct value for first item' do
       o = DummyOrder.first
-      o.dummy_line_items.first.price.should eq Monetize.parse('12.99')
+      expect(o.dummy_line_items.first.price).to eq Monetize.parse('12.99')
     end
 
     it 'should have correct value for first item' do
       o = DummyOrder.first
-      o.dummy_line_items.last.price.should eq Monetize.parse('14.99')
+      expect(o.dummy_line_items.last.price).to eq Monetize.parse('14.99')
     end
   end
 
@@ -348,15 +348,15 @@ describe Mongoid::MoneyField do
 
     it 'should have correct Money value for field 1' do
       dummy = DummyPrices.first
-      dummy.price1.should eq Monetize.parse('1.23')
+      expect(dummy.price1).to eq Monetize.parse('1.23')
     end
     it 'should have correct Money value for field 2' do
       dummy = DummyPrices.first
-      dummy.price2.should eq Monetize.parse('2.33')
+      expect(dummy.price2).to eq Monetize.parse('2.33')
     end
     it 'should have correct Money value for field 3' do
       dummy = DummyPrices.first
-      dummy.price3.should eq Monetize.parse('1')
+      expect(dummy.price3).to eq Monetize.parse('1')
     end
   end
 end
--- spec/ruby_money_spec.rb
+++ spec/ruby_money_spec.rb
@@ -15,48 +15,48 @@ describe Money do
 
   context "mongoize" do
     it "mongoizes correctly a Money object to a hash of cents and currency" do
-      priceable.price.cents.should == 100
-      priceable.price.currency.should == Money::Currency.find('EUR')
+      expect(priceable.price.cents).to eq(100)
+      expect(priceable.price.currency).to eq(Money::Currency.find('EUR'))
     end
 
     it "mongoizes correctly a Numeric object to a hash of cents and currency" do
-      priceable_from_num.price.cents.should == 100
-      priceable_from_num.price.currency.should == Money.default_currency
+      expect(priceable_from_num.price.cents).to eq(100)
+      expect(priceable_from_num.price.currency).to eq(Money.default_currency)
     end
 
     it "mongoizes correctly a String object to a hash of cents and currency" do
-      priceable_from_string.price.cents.should == 100
-      priceable_from_string.price.currency.should == Money::Currency.find('EUR')
+      expect(priceable_from_string.price.cents).to eq(100)
+      expect(priceable_from_string.price.currency).to eq(Money::Currency.find('EUR'))
     end
 
     it "mongoizes correctly a hash of cents and currency" do
-      priceable_from_hash.price.cents.should == 100
-      priceable_from_hash.price.currency.should == Money::Currency.find('EUR')
+      expect(priceable_from_hash.price.cents).to eq(100)
+      expect(priceable_from_hash.price.currency).to eq(Money::Currency.find('EUR'))
     end
 
     it "mongoizes correctly a HashWithIndifferentAccess of cents and currency" do
-      priceable_from_hash_with_indifferent_access.price.cents.should == 100
-      priceable_from_hash_with_indifferent_access.price.currency.should == Money::Currency.find('EUR')
+      expect(priceable_from_hash_with_indifferent_access.price.cents).to eq(100)
+      expect(priceable_from_hash_with_indifferent_access.price.currency).to eq(Money::Currency.find('EUR'))
     end
   end
 
   context "demongoize" do
     subject { Priceable.first.price }
-    it { should be_an_instance_of(Money) }
-    it { should == Money.new(100, 'EUR') }
+    it { is_expected.to be_an_instance_of(Money) }
+    it { is_expected.to eq(Money.new(100, 'EUR')) }
     it "returns nil if a nil value was stored" do
       nil_priceable = Priceable.create(:price => nil)
-      nil_priceable.price.should be_nil
+      expect(nil_priceable.price).to be_nil
     end
     it 'returns nil if an unknown value was stored' do
       zero_priceable = Priceable.create(:price => [])
-      zero_priceable.price.should be_nil
+      expect(zero_priceable.price).to be_nil
     end
   end
 
   context "evolve" do
     it "transforms correctly a Money object to a Mongo friendly value" do
-      Priceable.where(:price => Money.new(100, 'EUR')).first.should == priceable
+      expect(Priceable.where(:price => Money.new(100, 'EUR')).first).to eq(priceable)
     end
   end
 end
